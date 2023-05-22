dc8597566743e3cb8fffd40d053d40ec9b5652af
Alexandra Riano <alexandrariano@alexandras-air.lan>
2023-05-22 18:48:24 -0500

    added styling to log, reg and nav bar

--- client/src/components/UserProfile.js
+++ client/src/components/UserProfile.js
@@ -1,45 +1,42 @@
-import React, {useState, useEffect} from 'react';
-import axios from 'axios';
-import {useParams} from 'react-router-dom';
-import PostsByUser from '../components/PostsByUser';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { useParams } from "react-router-dom";
+import PostsByUser from "../components/PostsByUser";
 
 const UserProfile = (props) => {
-    const {id} = useParams();
-    const [user, setUser] = useState({});
-    const [loaded, setLoaded] = useState(false);
+	const { id } = useParams();
+	const [user, setUser] = useState({});
+	const [loaded, setLoaded] = useState(false);
 
-    useEffect(() => {
-        axios.get(`http://localhost:8000/api/user/${id}`)
-            .then((response) => {
-            console.log(response);
-            setUser(response.data);
-            setLoaded(true);
-            })
-            .catch((error) => {
-                console.log(error);
-            }) 
-    }, [])
+	useEffect(() => {
+		axios
+			.get(`http://localhost:8000/api/user/${id}`)
+			.then((response) => {
+				console.log(response);
+				setUser(response.data);
+				setLoaded(true);
+			})
+			.catch((error) => {
+				console.log(error);
+			});
+	}, []);
 
-    return(
-        <div>
-            <div>
-            {
-                user.image?
-                <img src={user.image} style={{width: "200px"}}/>:
-                <p>You haven't uploaded a profile picture!</p>
-            }
-            <p>{user.username}</p>
-            <p>Number of posts placeholder</p>
-            <p>Number of Followers placeholder</p>
-            <p>Number of Following placeholder</p>
-            {
-                loaded?
-                <PostsByUser user_id={user._id} />:
-                null
-            }
-            </div>
-        </div>
-    )
-}
+	return (
+		<div>
+			<div>
+				{user.image ? (
+					<img src={user.image} style={{ width: "200px" }} />
+				) : (
+					<p>You haven't uploaded a profile picture!</p>
+				)}
+				<p>{user.username}</p>
+				<p>Number of posts placeholder</p>
+				<p>Number of Followers placeholder</p>
+				<p>Number of Following placeholder</p>
+				{loaded ? <PostsByUser user_id={user._id} /> : null}
+			</div>
+		</div>
+	);
+};
 
 export default UserProfile;
